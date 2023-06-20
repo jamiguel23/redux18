0252254d4d92b15a127c966191ce34aa8bbdf65a
Matt Miguel <75647212+jamiguel23@users.noreply.github.com>
2023-06-19 22:49:20 -0700

    added component

---
+++ src/Greeting.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Greeting() {
+  return (
+   React.createElement('h2', {}, 'hello world again')
+   //the same as:
+   // <h2> hello world again </h2>
+  )
+}
